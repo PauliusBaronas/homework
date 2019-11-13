8613673670af6a611455773cac6b969cb1784d95
PauliusBaronas <aus10bbarp@gmail.com>
2019-11-13 18:33:12 +0200

    new

--- 11-13 ND 1 BE.php
+++ 11-13 ND 1 BE.php
@@ -4,16 +4,16 @@
 
 if (isset($_POST['username'], $_POST['password'])) {
 
-    if (is_numeric($_POST['username']) || is_numeric($_POST['password'])) {
+    if ($_POST['password'] == 'qwerty') {
+        $check = is_numeric($_POST['username']);
+        echo $check;
+        echo "slapta puslapio dalis";
+
+    } else {
 
         require('11-13 ND 1 FE.PHP');
         echo 'duomenys įvesti neteisingai';
 
-    } else {
-
-        $check = is_numeric($_POST['username']);
-        echo $check;
-        echo "slapta puslapio dalis";
     }
 
 }
